453ddc415b5730443eda16dcefc5713c142d6491
Mohamed Sallam <59128458+mohamed-sallam@users.noreply.github.com>
2023-04-25 03:07:18 +0200

    general/🌟/Improve temp main()

--- src/main.c
+++ src/main.c
@@ -3,8 +3,32 @@
 #include <avr/io.h>
 
 #include <seven_segments.h>
-#include <prayer_times.h>
+#include <led.h>
+#include <rtc.h>
 
+#include <prayer_times.h>
+#include <kuwaiti_hijri_date.h>
+
+uint8_t hijri_date[8];
+uint8_t gregorian_date[8];
+
+void swapHijriGregorianDate(uint8_t digits[8], uint8_t* const hijri_gregorian_led_index) 
+{
+    static uint8_t time_count = 0;
+    if (time_count == 0)
+    {
+        for (uint8_t i = 0; i < 8; ++i)
+            digits[i] = hijri_date[i];
+        *hijri_gregorian_led_index = 0;
+    }
+    else if (time_count == 128)
+    {
+        for (uint8_t i = 0; i < 8; ++i)
+            digits[i] = gregorian_date[i];
+        *hijri_gregorian_led_index = 1;
+    }
+    ++time_count;
+}
 
 int main() 
 {
@@ -28,12 +52,37 @@ int main()
     };
     PrayerTimes_init(&settings);
     uint8_t digits[NUMBER_OF_DIGITS];
-    PrayerTimes_get(julianDate, digits);
+    PrayerTimes_get(julianDate, &digits[12]);
     
+    Rtc_init(ClockTimeFormat_12h);
+    const Date date = {
+        .day = 0x06,
+        .date = 0x21,
+        .month = 0x04,
+        .year = 0x23
+    };
+    const Time time = {
+        .sec = 0x55,
+        .min = 0x59,
+        .hour = 0x11
+    };
     SevenSegments_init();
+    Rtc_setDateTime(&date, &time);
+    Rtc_getDate(gregorian_date);
+    KuwaitiHijriDate_calculate(julianDate, hijri_date);
+    Led leds[3] = {
+        {.group_index = 38, .index = 0},
+        {.group_index = 39, .index = 0},
+        {.group_index = 48, .index = 0}
+    };
+    leds[2].index = Rtc_getDay();
+    Led_init();
 
     while (1) 
     {
+        swapHijriGregorianDate(digits, &leds[0].index);
+        Rtc_getTime(&digits[8], &leds[1].index);
+        Led_litLeds(leds);
         SevenSegments_writeDigits(digits);
     }
 }
